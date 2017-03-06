9f2d9ea3a63a94591239e3ceb5604a8b0432aa05
mblank722 <mitchellblank@sbcglobal.net>
2017-03-06 15:53:04 -0800

    next v

--- js_fun/js_fun2.js
+++ js_fun/js_fun2.js
@@ -27,20 +27,24 @@ function stringReturnOne () {
 function caller(func0){
   if (typeof func0 === 'function')
     func0();
+  else {
+    console.log(func0)
+  }
 }
 
 caller(runningLogger);
+caller(4)
 
 // Medium: Write a function named myDoubleConsoleLog that has two parameters, if the arguments passed to the function are functions, console.log the value that each, when invoked, returns.
 //
 
-function myDoubleConsoleLog (func1, func2){
+function myDoubleConsoleLog (func1, func2{
   if (typeof func1 === 'function' && func2 === 'function')
     console.log(func1());
     console.log(func2());
 }
 
-
+myDoubleConsoleLog(runningLogger, multiplyByTen(9))
 // Hard: Write a function named caller2 that has one parameter. Have it console.log the string 'starting', wait 2 seconds, and then invokes the argument if the argument is a function. (setTimeout may be useful for this one.) The function should then console.log ‘ending?’ and return “interesting”. Invoke this function by passing it myDoubleConsoleLog.
 
 function caller2 (func3) {
