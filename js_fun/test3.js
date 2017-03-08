fd03c94fbb3acd7d0dbea121baaeea8fe8fd0e27
mblank722 <mitchellblank@sbcglobal.net>
2017-03-07 21:43:26 -0800

    next v

---
+++ js_fun/test3.js
@@ -0,0 +1,10 @@
+var questions = [
+   {question: "what is your name"},
+   {question: "How old are you"},
+   {question: "whats is your mothers name"},
+   {question: "where do work/or study"},
+   ];
+
+var match = _.filter(questions),function(words){ return words === "how"});
+
+console.log(match);
