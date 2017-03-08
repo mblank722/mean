8690e9023bda1b7ebeb8390816e24f06ceec6249
mblank722 <mitchellblank@sbcglobal.net>
2017-03-07 18:27:13 -0800

    next v

--- js_fun/deck_cards.js
+++ js_fun/deck_cards.js
@@ -17,23 +17,23 @@ function DeckConstructor(name, numWheels, numPass, speed) {
 
   DeckConstructor.prototype.reset = function (deck){
     var end_of_deck = false;
-    var suits = ['d', 'c', 'h', 's']
+    var suits = ['d', 'c', 'h', 's'];
     // while (!end_of_deck) {
-    //   var cnt = 0
+     var cnt = 0
       for (var i = 0; i < 4; i++) {
 
         for (var j = 0; j < 13; j++) {
           cnt++
           if (j < 11) {
-            deck [cnt] = j + suits[i]
+            this.deck [cnt] = j + suits[i]
           } else if (j = 11) {
-            deck [cnt] = 'j' + suits[i]
+            this.deck [cnt] = 'j' + suits[i]
           } else if (j = 12) {
-            deck [cnt] = 'q' + suits[i]
+            this.deck [cnt] = 'q' + suits[i]
           } else {
-            deck [cnt] = 'k' + suits[i]
+            this.deck [cnt] = 'k' + suits[i]
           }
-
+          console.log("cnt: " + cnt + " card: " + this.deck [cnt])
         }
 
       }
@@ -41,32 +41,7 @@ function DeckConstructor(name, numWheels, numPass, speed) {
   DeckConstructor.prototype.shuffle = function (deck){};
   DeckConstructor.prototype.deal = function (deck){};
 
+  }
 }
-// var schwinn=new VehicleConstructor('Bike',2,1,15);
-// schwinn.makeNoise=function(){console.log('ring, ring!')};
-// schwinn.move();
-// schwinn.checkMiles();
-// schwinn.getVin();
-// //console.log(schwinn)
-//
-// var accord=new VehicleConstructor('Sedan',2,4,70);
-// accord.makeNoise=function(){console.log('honk, honk!')};
-// accord.move();
-// accord.checkMiles();
-// //console.log(accord)
-//
-// var bus= new VehicleConstructor('Bus',8,14,40);
-// bus.makeNoise=function(){console.log('loud honk, honk!')};
-// bus.pickUp=function(new_pass){
-//   this.numPass+=new_pass;
-//   console.log('Passenger count: ' + this.numPass)
-// };
-// bus.pickUp(5);
-// bus.pickUp(1);
-// bus.move();
-// bus.checkMiles();
-// bus.move();
-// bus.checkMiles();
-// bus.getVin();
-//
-// //console.log(bus);
+deck1 = new DeckConstructor();
+deck1.reset();
