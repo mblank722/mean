11f55703ed0780a471fced13ae9bda25446e0b0b
mblank722 <mitchellblank@sbcglobal.net>
2017-03-08 12:19:42 -0800

    next v

--- js_fun/test3.js
+++ js_fun/test3.js
@@ -1,10 +1,19 @@
-var questions = [
-   {question: "what is your name"},
-   {question: "How old are you"},
-   {question: "whats is your mothers name"},
-   {question: "where do work/or study"},
-   ];
-
-var match = _.filter(questions),function(words){ return words === "how"});
-
-console.log(match);
+function Cat( cat_name ) {
+  var name = cat_name;
+  this.getName = function() {
+    return name;
+  };
+}
+//adding a method to the cat prototype
+Cat.prototype.sayHi = function(){
+  console.log('meow');
+};
+//adding properties to the cat prototype
+Cat.prototype.numLegs = 4;
+var muffin = new Cat('muffin');
+var biscuit = new Cat('biscuit');
+console.log(muffin, biscuit);
+//we access prototype properties the same way as we would access 'own' properties
+muffin.sayHi();
+biscuit.sayHi();
+console.log(muffin.numLegs);
