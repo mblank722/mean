88c43993991ab5badd612886d0cf5de02bd0ec74
mblank722 <mitchellblank@sbcglobal.net>
2017-03-07 17:08:57 -0800

    next v

--- js_fun/create_objects3.js
+++ js_fun/create_objects3.js
@@ -2,48 +2,69 @@
 function VehicleConstructor(name, numWheels, numPass, speed) {
 //PRIVATE
   var self=this;
-  var distance_travelled = 10;
+  var distance_travelled = 0;
+  var vin = 0;
   var updateDistanceTravelled = function(self){
-        // console.log("in func uDT - dt before: " + distance_travelled);
     distance_travelled = distance_travelled + self.speed
-    // console.log("in func uDT - dt after: " + distance_travelled);
-    // console.log("in func uDT - spd: " + self.speed)
+    return self
   }
-  var display_distance_travelled = function (){
-    self.distance_travelled;
+  var display_distance_travelled = function (self){
+    distance_travelled;
     console.log('distance_travelled: ' + distance_travelled);
+    return self
+  }
+  var createVin = function(){
+      var x = Math.floor(Math.random() * 100000000000);
+      // console.log('VIN: ' + x)
+    return x
+  }
+  var returnVin = function(self) {
+    console.log('VIN: ' + vin)
+    return self
   }
 //PUBLIC
   this.name = name;
   this.numWheels = numWheels;
   this.numPass = numPass;
   this.speed = speed;
-  this.makeNoise = function(){
+  vin = createVin();
+  VehicleConstructor.prototype.makeNoise = function(){
     console.log('buzz, buzz!');
-  };
-  this.move = function(){
+    return self
+  }
+  VehicleConstructor.prototype.move = function(){
     updateDistanceTravelled(self);
     this.makeNoise();
-  };
-  this.checkMiles = function(){
+    return self
+  }
+  VehicleConstructor.prototype.checkMiles = function(){
     display_distance_travelled();
-  };
+    return self
+  }
+  // VehicleConstructor.prototype.setVin = function(self){
+  //   createVin();
+  //   return self;
+  // }
+  VehicleConstructor.prototype.getVin = function(self) {
+    returnVin()
+    return self;
+  }
 }
 
-
-schwinn=new VehicleConstructor('Bike',2,1,15);
+var schwinn=new VehicleConstructor('Bike',2,1,15);
 schwinn.makeNoise=function(){console.log('ring, ring!')};
 schwinn.move();
 schwinn.checkMiles();
-console.log(schwinn)
+schwinn.getVin();
+//console.log(schwinn)
 
-accord=new VehicleConstructor('Sedan',2,4,70);
+var accord=new VehicleConstructor('Sedan',2,4,70);
 accord.makeNoise=function(){console.log('honk, honk!')};
 accord.move();
 accord.checkMiles();
 //console.log(accord)
 
-bus= new VehicleConstructor('Bus',8,14,40);
+var bus= new VehicleConstructor('Bus',8,14,40);
 bus.makeNoise=function(){console.log('loud honk, honk!')};
 bus.pickUp=function(new_pass){
   this.numPass+=new_pass;
@@ -55,5 +76,6 @@ bus.move();
 bus.checkMiles();
 bus.move();
 bus.checkMiles();
+bus.getVin();
 
 //console.log(bus);
