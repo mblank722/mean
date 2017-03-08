c3e5399f61f54281ce1f8c5f9db01996bbcaa101
mblank722 <mitchellblank@sbcglobal.net>
2017-03-08 12:24:58 -0800

    next v

--- js_fun/test3.js
+++ js_fun/test3.js
@@ -1,19 +1,11 @@
-function Cat( cat_name ) {
-  var name = cat_name;
-  this.getName = function() {
-    return name;
-  };
-}
-//adding a method to the cat prototype
-Cat.prototype.sayHi = function(){
-  console.log('meow');
-};
-//adding properties to the cat prototype
-Cat.prototype.numLegs = 4;
-var muffin = new Cat('muffin');
-var biscuit = new Cat('biscuit');
-console.log(muffin, biscuit);
-//we access prototype properties the same way as we would access 'own' properties
-muffin.sayHi();
-biscuit.sayHi();
-console.log(muffin.numLegs);
+console.log("NOW: ");
+console.log("Declaring and assigning variable 'ninja'.");
+var ninja = 'Libby';
+
+setTimeout( function myCallbackFunction(){
+  console.log("LATER: ")
+  console.log(ninja, "LATER"); }, 2000
+);
+
+console.log("Printing ninja value.");
+console.log(ninja, "NOW");
