cd8809f1ab24116d5dece6a84cfbb67938540cc1
mblank722 <mitchellblank@sbcglobal.net>
2017-03-09 09:41:02 -0800

    next v

--- node/cars_and_cats/app.js
+++ node/cars_and_cats/app.js
@@ -6,29 +6,38 @@ var server = http.createServer(function (request, response){
     // see what URL the clients are requesting:
     console.log('client request URL: ', request.url);
     // this is how we do routing:
-    if(request.url === '/') {
-        fs.readFile('index.html', 'utf8', function (errors, contents){
+    if(request.url === '/cars') {
+        fs.readFile('cars.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
             response.write(contents);  //  send response body
             response.end(); // finished!
         });
     }
 
-    else if(request.url === '/ninjas') {
-        fs.readFile('ninjas.html', 'utf8', function (errors, contents){
+    else if(request.url === '/cats') {
+        fs.readFile('cats.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
             response.write(contents);  //  send response body
             response.end(); // finished!
         });
     }
 
-    else if(request.url === '/dojos/new') {
-        fs.readFile('ninjas.html', 'utf8', function (errors, contents){
+    else if(request.url === '/cars/new') {
+        fs.readFile('cars_make.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
             response.write(contents);  //  send response body
             response.end(); // finished!
         });
     }
+
+    else if(request.url === '/stylesheets/style.css'){
+    fs.readFile('./stylesheets/style.css', 'utf8', function(errors, contents){
+     response.writeHead(200, {'Content-type': 'text/css'});
+     response.write(contents);
+     response.end();
+    })
+  }
+
     // request didn't match anything:
     else {
         response.writeHead(404);
@@ -36,6 +45,6 @@ var server = http.createServer(function (request, response){
     }
 });
 // tell your server which port to run on
-server.listen(6789);
+server.listen(7077);
 // print to terminal window
 console.log("Running in localhost at port 6789");
