94169fe5b8ffa77873fc14f655afc6eaab8f21c9
mblank722 <mitchellblank@sbcglobal.net>
2017-03-16 21:24:53 -0700

    next v

--- node/cars_and_cats/app.js
+++ node/cars_and_cats/app.js
@@ -6,31 +6,148 @@ var server = http.createServer(function (request, response){
     // see what URL the clients are requesting:
     console.log('client request URL: ', request.url);
     // this is how we do routing:
+    //request cars
     if(request.url === '/cars') {
-        fs.readFile('cars.html', 'utf8', function (errors, contents){
+        fs.readFile('views/cars.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
             response.write(contents);  //  send response body
             response.end(); // finished!
         });
     }
-
+    //provide redquested car images
+    else if(request.url === '/car1.jpg'){
+  // notice we won't include the utf8 encoding
+      fs.readFile('./images/cars/car1.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    else if(request.url === '/car2.jpg'){
+      fs.readFile('./images/cars/car2.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    else if(request.url === '/car3.jpg'){
+      fs.readFile('./images/cars/car3.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    else if(request.url === '/car4.jpg'){
+      fs.readFile('./images/cars/car4.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    else if(request.url === '/car5.jpg'){
+      fs.readFile('./images/cars/car5.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    else if(request.url === '/car6.jpg'){
+      fs.readFile('./images/cars/car6.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    else if(request.url === '/car7.jpg'){
+      fs.readFile('./images/cars/car7.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    else if(request.url === '/car8.jpg'){
+      fs.readFile('./images/cars/car8.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    // request cats
     else if(request.url === '/cats') {
-        fs.readFile('cats.html', 'utf8', function (errors, contents){
+        fs.readFile('views/cats.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
             response.write(contents);  //  send response body
             response.end(); // finished!
         });
     }
-
+    //provide redquested cat images
+    else if(request.url === '/cat1.jpg'){
+  // notice we won't include the utf8 encoding
+      fs.readFile('./images/cats/cat1.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    else if(request.url === '/cat2.jpg'){
+      fs.readFile('./images/cats/cat2.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    else if(request.url === '/cat3.jpg'){
+      fs.readFile('./images/cats/cat3.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    else if(request.url === '/cat4.jpg'){
+      fs.readFile('./images/cats/cat4.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    else if(request.url === '/cat5.jpg'){
+      fs.readFile('./images/cats/cat5.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    else if(request.url === '/cat6.jpg'){
+      fs.readFile('./images/cats/cat6.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    else if(request.url === '/cat7.jpg'){
+      fs.readFile('./images/cats/cat7.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    else if(request.url === '/cat8.jpg'){
+      fs.readFile('./images/cats/cat8.jpg', function(errors, contents){
+          response.writeHead(200, {'Content-type': 'image/jpg'});
+          response.write(contents);
+          response.end();
+      })
+    }
+    // request new car form
     else if(request.url === '/cars/new') {
-        fs.readFile('cars_make.html', 'utf8', function (errors, contents){
+        fs.readFile('views/cars_make.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
             response.write(contents);  //  send response body
             response.end(); // finished!
         });
     }
-
-    else if(request.url === '/stylesheets/style.css'){
+    // request style: style.css
+    else if(request.url === '/style.css'){
     fs.readFile('./stylesheets/style.css', 'utf8', function(errors, contents){
      response.writeHead(200, {'Content-type': 'text/css'});
      response.write(contents);
